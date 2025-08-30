bc85d8797590b532902747c8b39ec654b24c4020
LeoPonin <146754076+LeoPonin@users.noreply.github.com>
2025-08-30 10:27:51 +0700

    adjust week04

--- self-practice/week04/test2.js
+++ self-practice/week04/test2.js
@@ -1,3 +1,12 @@
 const words = ['mango', 'apple','bananas', 'pineapple']
 const upper = words.map(word => word.toUpperCase())
 console.log(upper)
+
+
+// const words = ['mango', 'apple','bananas', 'pineapple']
+// const upper = words.map(word => word.toUpperCase())
+// console.log(upper) 
+// output: [ [Function: toUpperCase],
+//   [Function: toUpperCase],
+//   [Function: toUpperCase],
+//   [Function: toUpperCase] ]
